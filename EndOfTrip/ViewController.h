147f89e7778783bb8645040f2c400c53f2c47eb7
hfz1991 <funzho@gmail.com>
2015-07-27 20:17:46 +1000

    Initial Commit

---
+++ EndOfTrip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EndOfTrip
+//
+//  Created by Charles He on 27/07/2015.
+//  Copyright (c) 2015 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EndOfTrip/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EndOfTrip
+//
+//  Created by Charles He on 27/07/2015.
+//  Copyright (c) 2015 Charles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
